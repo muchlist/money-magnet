4467c0b9b6900cd3870ffcc2d54d87e02bc9420b
muchlis <muchlis@majoo.id>
2022-05-06 21:08:06 +0700

    add migrations file

---
+++ migrations/000006_create_user_pocket.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "user_pocket" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "user_id" uuid,
+  "pocket_id" bigint
+);
+
+ALTER TABLE "user_pocket" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "user_pocket" ADD FOREIGN KEY ("pocket_id") REFERENCES "pockets" ("id");
